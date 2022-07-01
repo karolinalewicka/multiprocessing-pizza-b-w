1905e8d3af37aa7c3372789a15dc588ce48de0a6
Karolina <karolinalewicka36@gmail.com>
2022-07-01 18:42:24 +0200

    done1

--- main.py
+++ main.py
@@ -1,52 +1,34 @@
 import cv2
-import os, sys
+import os
 import argparse
-from multiprocessing import Process
+from multiprocessing import Pool
 
 
-path = './output'
+path = "./output"
 input = "./photos"
+
 
 def create_output_dir():
     if not os.path.exists(path):
         os.makedirs(path)
 
 
-
-
 def convert_to_gray(file):
-    gray_img = cv2.imread(f'./photos/{file}', cv2.IMREAD_GRAYSCALE)
-    cv2.imwrite(f'./output/{file}', gray_img)
+    gray_img = cv2.imread(f"./photos/{file}", cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(f"./output/{file}", gray_img)
 
 
 def main():
     create_output_dir()
     filenames = os.listdir(input)
-    my_parser = argparse.ArgumentParser(description='how many CPU is used')
+    my_parser = argparse.ArgumentParser(description="how many CPU is used")
 
-    my_parser.add_argument('cpu',
-                         type=int,
-                         help='number of CPU')
+    my_parser.add_argument("cpu", type=int, help="number of CPU")
 
     parsed_args = my_parser.parse_args()
 
-
-
-    if parsed_args.cpu > 1:
-        print(parsed_args.cpu)
-    else:
-        for file in filenames:
-            convert_to_gray(file)
-
-    # # if multi cpu
-    # # pool, map
-    # if parsed_args.multi_CPU > 1:
-    #     p = Process(target=convert_to_gray, args=(parsed_args,))
-    #     p.start()
-    #     p.join()
-    # else:
-    #     for file in filenames:
-    #         convert_to_gray(file)
+    with Pool(parsed_args.cpu) as p:
+        p.map(convert_to_gray, filenames)
 
 
 if __name__ == "__main__":
